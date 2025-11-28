94a4f3e95a1c25f4dd749fe9913316c157124bb6
sudiptabag2004 <bububag2@gmail.com>
2025-11-28 12:00:30 +0530

    nothing

--- obj/Debug/net7.0/BridgeLabzApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/BridgeLabzApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7935550bae07d52c4ef220fe56def797318b50b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27eaf01921c3c73ff34809d974da009cffd6cb9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BridgeLabzApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
